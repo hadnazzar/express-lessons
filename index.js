f98a102bc3760b17aeb31bb127c72e27425449fc
Melih Yumak <melih193@gmail.com>
2021-03-29 23:25:16 +0200

    feat(): express.static lesson 2

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const path = require("path");
 const app = express()
 const port = 3000
+
+// app.use(express.static('public'));
+// app.use(express.static('files'));
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
